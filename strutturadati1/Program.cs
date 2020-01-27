a4ce4a583bd43b13a50e7114e60b8dd28675eaa1
studenti <studenti@rosselli.loc>
2020-01-27 09:54:48 +0100

    Add project files.

---
+++ strutturadati1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace strutturadati1
+{
+    class Program
+    {
+        private const string file_prova = @"fileprova.txt";
+        private const string file = @"file.txt";
+        static void Main(string[] args)
+        {
+            List<double> list = new List<double>();
+            using (StreamReader filee = new StreamReader(file_prova))
+            {
+                string line;
+                while ((line = filee.ReadLine()) != null)
+                {
+                    double a = double.Parse(line);
+                    list.Add(a);
+                }
+
+            }
+
+
+        }
+    }
+}
